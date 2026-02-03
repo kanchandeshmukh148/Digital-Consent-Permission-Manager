9a4fb286ca7740878d44b3f9528a34cc6388eacc
kanchan deshmukh <kanchandeshmukh148@gmail.com>
2026-02-03 22:24:21 +0530

    adding js

--- JS/login.js
+++ JS/login.js
@@ -6,5 +6,24 @@ function validateLoginForm() {
     alert("Each field is mandatory");
     return false;
   }
+  else if(x=="admin@gmail.com" && y=="admin123")
+  {
+      document.location.href="adminDashboard.html";
+      return false;
+  }
+  else if(x=="user@gmail.com" && y=="user123")
+  {
+      document.location.href="userDashboard.html";
+      return false;
+  }
+  else if(x=="app@gmail.com" && y=="app123")
+  {
+      document.location.href="approverDashboard.html";
+      return false;
+  }
+  else {
+    alert("Invalid email or password");
+    return false;
+  }
 }
 
